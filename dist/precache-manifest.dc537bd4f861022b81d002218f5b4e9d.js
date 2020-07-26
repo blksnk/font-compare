83f73f4e53d3ce55c4de4d6e9376f59b9bde4e36
JN Veigel <art.jnveigel@gmail.com>
2020-07-26 17:23:22 +0200

    :see_no_evil: add dist directory

---
+++ dist/precache-manifest.dc537bd4f861022b81d002218f5b4e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d42c1f86396fa352dc",
+    "url": "/css/app.e4338f8d.css"
+  },
+  {
+    "revision": "77eac705ffc357150cc4692e8cf8812a",
+    "url": "/fonts/beausite.77eac705.woff"
+  },
+  {
+    "revision": "fb1f202885149d61e557e8fd8c864741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73d42c1f86396fa352dc",
+    "url": "/js/app.62a78d4d.js"
+  },
+  {
+    "revision": "6ba03684181d0a60feaa",
+    "url": "/js/chunk-vendors.8f86b568.js"
+  },
+  {
+    "revision": "61bbbb295ad64a8bf49967aad5a6d208",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
